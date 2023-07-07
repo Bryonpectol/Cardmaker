b932577f7df35bb98bc70ac33da5a86b039d722d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-07-06 22:37:59 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let Form = document.getElementById("form"):
 let Name = document.getElementById("name").value;
 let Select = document.getElementById("select");
+let CardContainer = document.getElementById("cardContainer");
 
 function Generate() {
+  let nameP= document.createElement("p");
+  nameP.textContent = "" + Name;
+
+  CardContainer.appendChild(nameP);
   
 };
